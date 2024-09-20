c1ff20256c9771411ba7e8f00a56a799cb899822
SalmaMohamedSaad <salmaabouamod@gmail.com>
2024-09-20 10:08:22 +0300

    Exercises from 1 to 12  completed

--- app.js
+++ app.js
@@ -202,3 +202,74 @@ const basicCalculator = (num1, num2, operation) => {
 }
 
 //console.log('Exercise 9 Result:', basicCalculator(10, 5, 'multiply'))
+
+/*
+Exercise 10: calculateGrade()
+
+Define a function called calculateGrade. 
+It should take a numerical score and return the corresponding letter 
+grade (A, B, C, D, F). 
+
+For example, 90 and above yields an 'A', 80-89 is a 'B', 
+and 70-79 is a 'C', 60-69 is a 'D' and anything lower than a 60 is an 'F'.
+
+Example: calculateGrade(100) should return A.
+
+Complete the exercise in the space below:
+*/
+const calculateGrade = (score) => {
+  if (score > 100 || score < 1) {
+    return 'Invalid Score'
+  } else if (score >= 90 && score <= 100) {
+    return 'A'
+  } else if (score >= 80 && score <= 89) {
+    return 'B'
+  } else if (score >= 70 && score <= 79) {
+    return 'C'
+  } else if (score >= 60 && score <= 69) {
+    return 'D'
+  } else {
+    return 'F'
+  }
+}
+
+//console.log('Exercise 10 Result:', calculateGrade(60))
+
+/*
+Exercise 11: createUsername()
+
+Define a function called createUsername. 
+It should take a first name and a last name and return a username. 
+
+The username should be a combination of the following:
+- The first three letters of the first name.
+- The first three letters of the last name.
+- The total character count of the first and last name combined.
+
+Example: createUsername('Samantha', 'Green') should return 'SamGre13'.
+
+Complete the exercise in the space below:
+*/
+const createUsername = (firstName, lastName) => {
+  let userName = firstName.substr(0, 3) + lastName.substr(0, 3)
+  let charCount = firstName.length + lastName.length
+  return userName + charCount
+}
+//console.log('Exercise 11 Result:', createUsername('Samantha', 'Green'))
+
+/*
+Exercise 12: numArgs()
+
+Challenge yourself with numArgs. 
+This function should return the count of arguments passed to it when called.
+
+Complete the exercise in the space below:
+*/
+// let numArgs = (a, b, c) => {
+//   console.log(arguments.length)
+// }
+function numArgs(a, b, c) {
+  return console.log('Exercise 12 Result:', arguments.length)
+}
+numArgs(1, 4, 5, 5)
+// console.log('Exercise 12 Result:', numArgs(1, 4, 5))
